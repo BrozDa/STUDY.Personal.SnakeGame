4656c76b807ce4f76046b30df6a91bbbdce5e448
Daniel <Daniel@DESKTOP-EICR6RV>
2025-01-16 15:00:17 +0100

    initial implementation of SnakeBodyPart and Snake

---
+++ STUDY.Personal.SnakeGame/Snake.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STUDY.Personal.SnakeGame
+{
+    internal class Snake
+    {
+        (int, int) startingCoords = (10, 10);
+        private List<SnakeBodyPart> SnakeBody;
+        private SnakeBodyPart SnakeHead;
+        private SnakeBodyPart SnakeTail;
+        private int SnakeLength = 1;
+
+        public Snake()
+        {
+            SnakeBody = new List<SnakeBodyPart>() { new SnakeBodyPart(startingCoords) };
+            SnakeHead = SnakeBody[0];
+            SnakeTail = SnakeBody[SnakeLength - 1];
+        }
+        public void testPrint()
+        {
+            foreach (SnakeBodyPart sbp in SnakeBody)
+            {
+                Console.WriteLine(sbp.ToString());
+
+            }
+        }
+    }
+}
---
+++ STUDY.Personal.SnakeGame/SnakeBodyPart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STUDY.Personal.SnakeGame
+{
+    internal class SnakeBodyPart
+    {
+        (int x , int y) BodyPart;
+
+        public SnakeBodyPart((int, int) coords)
+        {
+            BodyPart = coords;
+        }
+        public override string ToString()
+        {
+            return new string($"x: {BodyPart.x}, y: {BodyPart.y}");
+        }
+    }
+}
