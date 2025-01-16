4826d103ec16e66e6f1d121749755b8478dac22f
Daniel <Daniel@DESKTOP-EICR6RV>
2025-01-16 14:30:26 +0100

    initialization of project

---
+++ STUDY.Personal.SnakeGame/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace STUDY.Personal.SnakeGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
