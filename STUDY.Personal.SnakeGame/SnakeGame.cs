bce4367234dcd20464b8909ebae7c7fb92621414
Daniel <Daniel@DESKTOP-EICR6RV>
2025-01-18 15:23:20 +0100

    user input reaction implemented

--- STUDY.Personal.SnakeGame/SnakeGame.cs
+++ STUDY.Personal.SnakeGame/SnakeGame.cs
@@ -8,12 +8,15 @@ namespace STUDY.Personal.SnakeGame
 {
     internal class SnakeGame
     {
-        private readonly int _timerTick = 1000;
+        private readonly int _timerTick = 200;
         private System.Timers.Timer _timer;
         private readonly char _snakeChar = '@';
         private GameBoard gameBoard { get; set; }
         private Snake snake { get; set; }
+        private Direction defaultDirection = Direction.Right;
 
+        private InputManager inputManager { get; set; }
+        private bool snakeAlive = true;
         public SnakeGame()
         {
             Console.CursorVisible = false;
@@ -22,17 +25,30 @@ namespace STUDY.Personal.SnakeGame
             gameBoard = new GameBoard();
             gameBoard.PrintBorder();
 
-            snake = new Snake(gameBoard);
+            inputManager = new InputManager(defaultDirection);
+
+            snake = new Snake(gameBoard, inputManager, defaultDirection);
             snake.PrintSnake(_snakeChar);
+
+            
+
+            _timer = new System.Timers.Timer();
             SetupTimer(_timerTick);
+        }
+        public void PlayGame()
+        {
+            while (snakeAlive) { 
+                
+            }
         }
         public void SetupTimer(int timerTick)
         {
-            _timer = new System.Timers.Timer();
+            
             _timer.Enabled = true;
             _timer.AutoReset = true;
             _timer.Interval = timerTick;
-            _timer.Elapsed += new System.Timers.ElapsedEventHandler(delegate { snake.PrintSnake(_snakeChar); });
+            _timer.Elapsed += new System.Timers.ElapsedEventHandler(delegate { snake.PrintSnake(_snakeChar);});
+
             
         }
         
--- STUDY.Personal.SnakeGame/obj/Debug/net8.0/STUDY.Personal.SnakeGame.AssemblyInfo.cs
+++ STUDY.Personal.SnakeGame/obj/Debug/net8.0/STUDY.Personal.SnakeGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("STUDY.Personal.SnakeGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4656c76b807ce4f76046b30df6a91bbbdce5e448")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65c74cad2d2c3f901a2097391984fa2ccc82094b")]
 [assembly: System.Reflection.AssemblyProductAttribute("STUDY.Personal.SnakeGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("STUDY.Personal.SnakeGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
